c425dd6f1e59c17586d0a4f50981a16600970f81
drowning-in-codes <bukalala174@gmail.com>
2023-07-08 15:55:32 +0800

    first set up

---
+++ capricious-crater/src/consts.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: drowning-in-codes bukalala174@gmail.com
+ * @Date: 2023-07-08 14:50:52
+ * @LastEditors: drowning-in-codes bukalala174@gmail.com
+ * @LastEditTime: 2023-07-08 15:46:48
+ * @FilePath: \my_astro\capricious-crater\src\consts.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Proanimer-My personal website.';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
